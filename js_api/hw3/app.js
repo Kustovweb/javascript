380f24a10f71fda56928767cc638e1950b4d2f2f
zk <kustovweb@gmail.com>
2024-05-26 22:45:21 +0700

    js_api

--- js_api/hw3/app.js
+++ js_api/hw3/app.js
@@ -3,7 +3,7 @@ const root = document.querySelector("#root");
 const nameAuthor = document.querySelector(".name-author");
 const countLike = document.querySelector("#count-like");
 const buttonLike = document.querySelector(".like");
-
+let likePerson = [];
 const img = document.createElement('img');
 (async () => {
     const res = await fetch(`https://api.unsplash.com/photos/random?client_id=${apiKey}`);
@@ -11,7 +11,6 @@ const img = document.createElement('img');
         return res.json();
     }
 })().then(data => {
-
     img.width = 800;
     img.height = 600;
     img.alt = data.alt_description;
@@ -21,9 +20,18 @@ const img = document.createElement('img');
     nameAuthor.textContent = `Автор: ${data.user.username}`
 });
 
-
+if (localStorage.getItem("unsplashPhoto")) {
+    const likes = JSON.parse(localStorage.getItem("unsplashPhoto"));
+    likePerson = JSON.parse(localStorage.getItem("unsplashPhoto"));
+    likes.forEach(obj => {
+        if (img.id === obj.like) {
+            countLike.textContent = obj.like;
+        }
+    });
+}
 buttonLike.addEventListener("click", function () {
     let num = 0;
     countLike.textContent = num + 1;
-    localStorage.setItem(img.id, +countLike.textContent)
-});
+    likePerson.push({ id: img.id, like: +countLike.textContent });
+    localStorage.setItem('unsplashPhoto', JSON.stringify(likePerson))
+}, { once: true });
