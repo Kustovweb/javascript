f8402166a40b5effb94739a88e5bf342b95350af
zk <kustovweb@gmail.com>
2024-05-28 23:28:57 +0700

    vuejs hw1

---
+++ vuejs/geekbrainsVue/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import hw1 from './components/hw1.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <hw1 />
+  </div>
+</template>
+
+<style scoped>
+
+.wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
+</style>
---
+++ vuejs/geekbrainsVue/src/components/hw1.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hello Vue!!!',
+                items: ["OldElement1", "OldElement2"]
+            }
+        },
+        methods: {
+            reverse() {
+                const text = this.message.split('').reverse().join('');
+                this.message = text;
+            },
+            addElementList() {
+                this.items.push('Новый элемент списка');
+            },
+            removeItem(index) {
+                this.items.splice(index, 1);
+            }
+        },
+    }
+</script>
+
+
+<template>
+    <div class="message">{{ message }}</div>
+    <button @click="reverse">Перевернуть текст</button>
+    <ul v-for="(element,index) in items" :key="index">
+        <li @click="removeItem(index)">{{ element }}</li>
+    </ul>
+    <button @click = "addElementList">Добавить элемент списка</button>
+</template>
+
+<style scoped>
+
+</style>
