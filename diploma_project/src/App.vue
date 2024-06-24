881139334a606999e39350fe9e0d5e423fcc409f
zk <kustovweb@gmail.com>
2024-06-24 15:18:38 +0700

    diplom

--- diploma_project/src/App.vue
+++ diploma_project/src/App.vue
@@ -5,13 +5,16 @@ import SearchNavBar from '@/components/SearchNavBar.vue'
 </script>
 
 <template>
-  <h1 class="font-bold text-2xl text-center mt-10">Поиск вакансий по России с автоматическим определением местоположения
-  </h1>
-  <div class="container mx-auto flex justify-center my-8">
-    <router-link to="/PageMain" class="self-start border border-slate-300 hover:border-indigo-300 py-2 px-6
+  <div class="sticky top-0 bg-white py-1 dark:border-gray-600">
+    <h1 class="font-bold text-2xl text-center mt-6">Поиск вакансий по России с автоматическим определением
+      местоположения
+    </h1>
+    <div class="container mx-auto flex justify-center my-4">
+      <router-link to="/PageMain" class="self-start border border-slate-300 hover:border-indigo-300 py-2 px-6
   rounded-full ml-8">Поиск</router-link>
-    <router-link to="/PageBookmarks" class="self-start border border-slate-300 hover:border-indigo-300 py-2 px-6
+      <router-link to="/PageBookmarks" class="self-start border border-slate-300 hover:border-indigo-300 py-2 px-6
   rounded-full ml-8">Закладки</router-link>
+    </div>
   </div>
   <router-view></router-view>
 </template>
--- diploma_project/src/pages/PageBookmarks.vue
+++ diploma_project/src/pages/PageBookmarks.vue
@@ -1,12 +1,8 @@
 <template>
-    <h1 class="text-center font-semibold text-3xl">Закладки</h1>
-    <div class="container mx-auto">
-        <div v-if="getDataArrLength == 0">
-            <p>
-                Закладок нет
-            </p>
-        </div>
-        <p class="text-center mt-4">Закладок: {{ getDataArrLength }}</p>
+    <h1 v-if="getDataArrLength > 0" class="text-center font-semibold text-3xl">Ваши закладки</h1>
+    <p v-else-if="getDataArrLength == 0" class="text-center my-4 text-red-500 text-xl">Закладок нет</p>
+    <p class="text-center mt-4">Закладок: {{ getDataArrLength }}</p>
+    <div class="container mx-auto mb-4">
         <div v-for="item in dataArr" :key="item.id" class="border-2 rounded-xl px-2 py-2 mt-5  w-2/3 mx-auto"
             :class="[isBookmark ? 'border-green-500/100' : 'border-indigo-500/100']">
             <p>Профессия: {{ item['job-name'] }}</p>
@@ -20,7 +16,7 @@
             <button @click="removeVacancy(item.id)"
                 class="ml-4 my-4 border-2 border-red-500/100 rounded-xl px-2 py-2 transition ease-in-out delay-150 hover:scale-110  duration-300">Удалить
                 из закладок</button>
-            <p class="my-4 text-gray-900">Заметка: {{ item.note }}</p>
+            <p class="my-4 text-gray-900 border-t-blue-400">Заметка: {{ item.note }}</p>
         </div>
     </div>
 
